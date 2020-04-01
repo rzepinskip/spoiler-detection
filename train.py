ee9342ce92ee6b9b486561a5bb8a55c4b4636136
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2020-04-01 14:07:08 +0200

    Add other Goodreads datareaders

--- train.py
+++ train.py
@@ -2,7 +2,11 @@ from argparse import ArgumentParser
 
 from pytorch_lightning import Trainer
 
-from spoiler_detection.datasets import GoodreadsSingleSentenceDataset
+from spoiler_detection.datasets import (
+    GoodreadsMultiSentenceDataset,
+    GoodreadsSingleSentenceDataset,
+    GoodreadsSscDataset,
+)
 from spoiler_detection.loggers import ResumableWandbLogger
 from spoiler_detection.models import BasicModel, PretrainedSingleSentenceModel
 
@@ -11,7 +15,11 @@ MODELS = {
     "PretrainedSingleSentenceModel": PretrainedSingleSentenceModel,
 }
 
-DATASETS = {"GoodreadsSingleSentenceDataset": GoodreadsSingleSentenceDataset}
+DATASETS = {
+    "GoodreadsMultiSentenceDataset": GoodreadsMultiSentenceDataset,
+    "GoodreadsSingleSentenceDataset": GoodreadsSingleSentenceDataset,
+    "GoodreadsSscDataset": GoodreadsSscDataset,
+}
 
 
 def main(args):
