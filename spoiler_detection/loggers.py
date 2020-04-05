e1287ed1de6ac2f83c53ddcfb006765b6680ce90
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2020-04-05 17:18:58 +0200

    Remove unused logger

--- spoiler_detection/loggers.py
+++ spoiler_detection/loggers.py
@@ -2,44 +2,7 @@ import glob
 import os
 
 import wandb
-from pytorch_lightning.loggers import TestTubeLogger, WandbLogger
-
-
-class ResumableTestTubeLogger(TestTubeLogger):
-    def __init__(self, version=None):
-        if version is None:
-            version = max(self.__get_last_experiment_version(), 0)
-        super(ResumableTestTubeLogger, self).__init__(
-            save_dir="lightning_logs", version=version
-        )
-
-    def get_last_checkpoint(self):
-        if self._experiment is None:
-            return None
-
-        last_version_checkpoints_folder = os.path.join(
-            "lightning_logs",
-            "default",
-            f"version_{self.__get_last_experiment_version()}",
-            "checkpoints",
-        )
-
-        return sorted(
-            glob.glob(os.path.join(last_version_checkpoints_folder, "*.ckpt",))
-        )[-1]
-
-    def __get_last_experiment_version(self):
-        try:
-            exp_cache_file = os.path.join("lightning_logs", "default")
-            last_version = -1
-            for f in os.listdir(exp_cache_file):
-                if "version_" in f:
-                    file_parts = f.split("_")
-                    version = int(file_parts[-1])
-                    last_version = max(last_version, version)
-            return last_version
-        except Exception as e:
-            return -1
+from pytorch_lightning.loggers import WandbLogger
 
 
 class ResumableWandbLogger(WandbLogger):
