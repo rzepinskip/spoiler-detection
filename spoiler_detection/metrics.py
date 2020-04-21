1b672221d73c037949a2e243c5ecfc11b4f56da0
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2020-04-21 22:03:50 +0200

    Add more metrics

--- spoiler_detection/metrics.py
+++ spoiler_detection/metrics.py
@@ -14,14 +14,20 @@ def get_metrics(probs, truth, prefix):
     probs, truth = probs.cpu().detach(), truth.cpu().detach()
     predicted = torch.argmax(probs, dim=1)
     acc = metrics.accuracy_score(truth, predicted)
+    f1 = metrics.f1_score(truth, predicted)
     if truth.min() != truth.max():
         auc = metrics.roc_auc_score(truth, probs[:, 1],)
+        precision, recall, _ = metrics.precision_recall_curve(truth, probs[:, 1],)
+        pr_auc = metrics.auc(recall, precision)
     else:
         auc = float("nan")
+        pr_auc = float("nan")
 
     return {
-        f"avg_{prefix}_acc": torch.tensor(acc),
         f"avg_{prefix}_auc": torch.tensor(auc),
+        f"avg_{prefix}_acc": torch.tensor(acc),
+        f"avg_{prefix}_pr-auc": torch.tensor(pr_auc),
+        f"avg_{prefix}_f1": torch.tensor(f1),
         f"{prefix}_pred_pos": sum(predicted == 1) / float(len(predicted)),
         f"{prefix}_true_pos": sum(truth == 1) / float(len(predicted)),
     }
