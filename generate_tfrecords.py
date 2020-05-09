0acfb2462987d4bb7c8b102ec1155648ed65d4d1
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2020-05-09 13:21:14 +0200

    Add more options to tf records generation

--- generate_tfrecords.py
+++ generate_tfrecords.py
@@ -9,21 +9,25 @@ from spoiler_detection.datasets import (
 
 model_types = ["bert-base-cased", "bert-base-uncased", "albert-base-v2", "roberta-base"]
 dataset_classes = [GoodreadsSingleDataset]
+max_sentences_limits = [5]
+max_length_limits = [96]
 
 for model_type in model_types:
     for dataset_class in dataset_classes:
-        parser = ArgumentParser(add_help=False)
+        for max_length in max_length_limits:
+            for max_sentences in max_sentences_limits:
+                parser = ArgumentParser(add_help=False)
 
-        parser.add_argument("--model_type", default=model_type, type=str)
-        parser.add_argument("--max_length", default=96, type=int)
-        parser.add_argument("--max_sentences", default=5, type=int)
-        args = parser.parse_args([])
+                parser.add_argument("--model_type", default=model_type, type=str)
+                parser.add_argument("--max_length", default=max_length, type=int)
+                parser.add_argument("--max_sentences", default=max_sentences, type=int)
+                args = parser.parse_args([])
 
-        dataset = dataset_class(hparams=args)
-        dataset_types = ["train", "val"]
-        for dataset_type in dataset_types:
-            if not os.path.isfile(dataset.get_file_name(dataset_type)):
-                print(
-                    f"[{model_type}][{dataset_class.__name__}][{dataset_type}] Tokenizing..."
-                )
-                dataset.write_dataset(dataset_type)
+                dataset = dataset_class(hparams=args)
+                dataset_types = ["train", "val"]
+                for dataset_type in dataset_types:
+                    if not os.path.isfile(dataset.get_file_name(dataset_type)):
+                        print(
+                            f"[{model_type}][{dataset_class.__name__}][{dataset_type}] Tokenizing..."
+                        )
+                        dataset.write_dataset(dataset_type)
